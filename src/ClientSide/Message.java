c9b3129c1ba06c73861f9a6bacd2ea5d75328929
sarthakmanna <sarthakmannaofficial@gmail.com>
2019-05-16 14:15:07 +0530

    Added getter and setter functions in Message.java and User.java at the ClientSide

--- src/ClientSide/Message.java
+++ src/ClientSide/Message.java
@@ -1,14 +1,46 @@
 package ClientSide;
 
 public class Message {
-    String userInvolved, message;
-    long timeStamp;
-    boolean isSent;
+    private String userInvolved, message;
+    private long timeStamp;
+    private boolean isSent;
 
-    Message(String username, String time, String msg, String isSendType) {
+    public Message(String username, String time, String msg, String isSendType) {
         userInvolved = username;
         timeStamp = Long.parseLong(time);
         message = msg;
         isSent = isSendType.equals(ClientAPI.YES);
+    }
+
+    public String getUserInvolved() {
+        return userInvolved;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public boolean isSent() {
+        return isSent;
+    }
+
+    public void setUserInvolved(String userInvolved) {
+        this.userInvolved = userInvolved;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public void setSent(boolean sent) {
+        isSent = sent;
     }
 }
--- src/ClientSide/User.java
+++ src/ClientSide/User.java
@@ -1,13 +1,36 @@
 package ClientSide;
 
 public class User {
-    String USERNAME;
-    String STATUS;
-    boolean HAS_CHATTED_BEFORE;
+    private String USERNAME, STATUS;
+    private boolean HAS_CHATTED_BEFORE;
 
-    User(String name, String status, String chattedBefore) {
+    public User(String name, String status, String chattedBefore) {
         USERNAME = name;
         STATUS = status;
         HAS_CHATTED_BEFORE = chattedBefore.equals(ClientAPI.YES);
+    }
+
+    public String getUSERNAME() {
+        return USERNAME;
+    }
+
+    public String getSTATUS() {
+        return STATUS;
+    }
+
+    public boolean isHAS_CHATTED_BEFORE() {
+        return HAS_CHATTED_BEFORE;
+    }
+
+    public void setUSERNAME(String USERNAME) {
+        this.USERNAME = USERNAME;
+    }
+
+    public void setSTATUS(String STATUS) {
+        this.STATUS = STATUS;
+    }
+
+    public void setHAS_CHATTED_BEFORE(boolean HAS_CHATTED_BEFORE) {
+        this.HAS_CHATTED_BEFORE = HAS_CHATTED_BEFORE;
     }
 }
